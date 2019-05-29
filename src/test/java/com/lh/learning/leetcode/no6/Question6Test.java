3b9b3a5ca553ada83f465d8b6711f101614e1105
loneyfall <312988970@qq.com>
2019-05-29 10:20:39 +0800

    Merge branch 'master-mergin'
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/com/lh/learning/leetcode/no6/Question6Test.java
@@ -0,0 +1,26 @@
+package com.lh.learning.leetcode.no6;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @auther: loneyfall
+ * @date: 2019/5/28
+ * @description:
+ */
+public class Question6Test {
+    private static Question6 question6;
+
+    @BeforeClass
+    public static void beforeClass() {
+        question6 = new Question6();
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(question6.convert("LEETCODEISHIRING", 3), "LCIRETOESIIGEDHN");
+        Assert.assertEquals(question6.convert("LEETCODEISHIRING", 4), "LDREOEIIECIHNTSG");
+        Assert.assertEquals(question6.convert("ABC", 1), "ABC");
+    }
+}

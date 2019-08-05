18aeee33d95644f06080fc3afa2cf73865689ea9
loneyfall <312988970@qq.com>
2019-08-05 18:00:17 +0800

    no9 commit

---
+++ src/test/java/com/lh/learning/leetcode/no9/Question9Test.java
@@ -0,0 +1,29 @@
+package com.lh.learning.leetcode.no9;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @auther: loneyfall
+ * @date: 2019/8/5
+ * @description:
+ */
+public class Question9Test {
+    private static Question9 question9;
+
+    @BeforeClass
+    public static void beforeClass() {
+        question9 = new Question9();
+    }
+
+    @Test
+    public void test() {
+//        Assert.assertFalse(question9.isPalindrome(123456));
+//        Assert.assertTrue(question9.isPalindrome(12321));
+//        Assert.assertFalse(question9.isPalindrome(-12321));
+//        Assert.assertFalse(question9.isPalindrome(10));
+//        Assert.assertTrue(question9.isPalindrome(121));
+        Assert.assertTrue(question9.isPalindrome(1410110141));
+    }
+}

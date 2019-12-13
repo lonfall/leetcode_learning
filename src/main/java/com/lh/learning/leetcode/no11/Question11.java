fa29d54407fa86b5b279333ea0c9da10a77e175b
loneyfall <312988970@qq.com>
2019-12-13 16:48:45 +0800

    no11注释

--- src/main/java/com/lh/learning/leetcode/no11/Question11.java
+++ src/main/java/com/lh/learning/leetcode/no11/Question11.java
@@ -1,9 +1,13 @@
 package com.lh.learning.leetcode.no11;
 
 /**
+ * 双指针法，从最大长度即数组最前与最后开始
+ * 每次舍弃较短值向内移动指针，直到指针相遇
+ * 每次计算面积，更新最大值
+ *
  * @auther: loneyfall
  * @date: 2019/12/13
- * @description:
+ * @description: 盛最多水的容器
  */
 public class Question11 {
 

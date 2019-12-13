c954373ec40d3949996c2989fa98bb0c6e0551c0
loneyfall <312988970@qq.com>
2019-12-13 16:44:24 +0800

    no11

--- src/main/java/com/lh/learning/leetcode/no11/Question11.java
+++ src/main/java/com/lh/learning/leetcode/no11/Question11.java
@@ -6,4 +6,31 @@ package com.lh.learning.leetcode.no11;
  * @description:
  */
 public class Question11 {
+
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int max_area = 0;
+        while (i < j) {
+            int a = height[i];
+            int b = height[j];
+            int area = (j - i) * min(a, b);
+            if (area > max_area) {
+                max_area = area;
+            }
+            if (a == b) {
+                i++;
+                j--;
+            } else if (a > b) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return max_area;
+    }
+
+    private int min(int a, int b) {
+        return a > b ? b : a;
+    }
 }
--- src/test/java/com/lh/learning/leetcode/no11/Question11Test.java
+++ src/test/java/com/lh/learning/leetcode/no11/Question11Test.java
@@ -1,9 +1,24 @@
 package com.lh.learning.leetcode.no11;
 
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 /**
  * @auther: loneyfall
  * @date: 2019/12/13
  * @description:
  */
 public class Question11Test {
+    private static Question11 question11;
+
+    @BeforeClass
+    public static void beforeClass() {
+        question11 = new Question11();
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(question11.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}), 49);
+    }
 }

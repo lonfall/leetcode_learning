b34f7e3303727a0c5cecf8c8e37618b14743ff12
loneyfall <312988970@qq.com>
2019-12-16 10:15:24 +0800

    no12

--- src/main/java/com/lh/learning/leetcode/no12/Question12.java
+++ src/main/java/com/lh/learning/leetcode/no12/Question12.java
@@ -1,9 +1,13 @@
 package com.lh.learning.leetcode.no12;
 
 /**
+ * 罗马数字每一位计数的规则一样
+ * 只是表示单位的字符不一样
+ * 这里取每一位的数字套入公式计算即可
+ *
  * @auther: loneyfall
  * @date: 2019/12/13
- * @description:
+ * @description: 整数转罗马数字
  */
 public class Question12 {
     public String intToRoman(int num) {
@@ -11,7 +15,8 @@ public class Question12 {
         int ten = num / 10 % 10;
         int hundred = num / 100 % 10;
         int thousand = num / 1000 % 10;
-        return "";
+        return formart(thousand, 'M', '\0', '\0') + formart(hundred, 'C', 'D', 'M')
+                + formart(ten, 'X', 'L', 'C') + formart(unit, 'I', 'V', 'X');
     }
 
     private String formart(int num, char unit, char helf, char ten) {
@@ -21,7 +26,14 @@ public class Question12 {
                 result += unit;
             }
         } else if (num == 4) {
-
+            result += unit + "" + helf;
+        } else if (num < 9) {
+            result += helf;
+            for (int i = 0; i < num - 5; i++) {
+                result += unit;
+            }
+        } else if (num == 9) {
+            result += unit + "" + ten;
         }
         return result;
     }

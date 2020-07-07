bb5e0f637f37172948b7b9b3eb7129b01cbfa71c
loneyfall <312988970@qq.com>
2020-07-07 09:33:59 +0800

    no24

---
+++ src/test/java/com/lh/learning/leetcode/no24/Question24Test.java
@@ -0,0 +1,24 @@
+package com.lh.learning.leetcode.no24;
+
+import com.lh.learning.leetcode.util.ListNode;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @auther: loneyfall
+ * @date: 2020/7/3
+ * @description:
+ */
+public class Question24Test {
+    private static Question24 question24;
+
+    @BeforeClass
+    public static void beforeClass() {
+        question24 = new Question24();
+    }
+
+    @Test
+    public void test() {
+        System.out.println(question24.swapPairs(ListNode.init(new int[]{1, 2, 3, 4, 5})));
+    }
+}

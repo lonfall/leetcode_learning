4cd9d53dd4c433f73d8fe1be66f256b6dba8828e
loneyfall <312988970@qq.com>
2020-07-08 09:57:44 +0800

    no26

---
+++ src/main/java/com/lh/learning/leetcode/no26/Question26.java
@@ -0,0 +1,23 @@
+package com.lh.learning.leetcode.no26;
+
+/**
+ * @auther: loneyfall
+ * @date: 2020/7/8
+ * @description: 删除排序数组中的重复项
+ */
+public class Question26 {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] == nums[j]) {
+                continue;
+            }
+            i++;
+            nums[i] = nums[j];
+        }
+        return i + 1;
+    }
+}

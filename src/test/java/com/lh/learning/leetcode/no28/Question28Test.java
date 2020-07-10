75701549a9c597a40e7bb650931cb49f369c443e
loneyfall <312988970@qq.com>
2020-07-10 10:08:17 +0800

    no28

---
+++ src/test/java/com/lh/learning/leetcode/no28/Question28Test.java
@@ -0,0 +1,23 @@
+package com.lh.learning.leetcode.no28;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @auther: loneyfall
+ * @date: 2020/7/10
+ * @description:
+ */
+public class Question28Test {
+    private static Question28 question28;
+
+    @BeforeClass
+    public static void beforeClass() {
+        question28 = new Question28();
+    }
+
+    @Test
+    public void test() {
+        System.out.println(question28.strStr("aaaaa", "bba"));
+    }
+}

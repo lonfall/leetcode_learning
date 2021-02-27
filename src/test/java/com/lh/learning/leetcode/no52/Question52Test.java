901b0e1f45c278f93ec8d19c69010fb1b000f42d
loneyfall <312988970@qq.com>
2021-02-27 16:57:32 +0800

    no52

---
+++ src/test/java/com/lh/learning/leetcode/no52/Question52Test.java
@@ -0,0 +1,23 @@
+package com.lh.learning.leetcode.no52;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @auther: loneyfall
+ * @date: 2021/2/27
+ * @description:
+ */
+public class Question52Test {
+    private static Question52 question52;
+
+    @BeforeClass
+    public static void beforeClass() {
+        question52 = new Question52();
+    }
+
+    @Test
+    public void test() {
+        System.out.println(question52.totalNQueens(7));
+    }
+}

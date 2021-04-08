bcae168c275374ae4659aff97008f43c2b1c6307
loneyfall <312988970@qq.com>
2021-04-08 17:22:56 +0800

    no65

---
+++ src/main/java/com/lh/learning/leetcode/no65/Question65.java
@@ -0,0 +1,9 @@
+package com.lh.learning.leetcode.no65;
+
+/**
+ * @auther: loneyfall
+ * @date: 2021/4/8
+ * @description:
+ */
+public class Question65 {
+}
---
+++ src/test/java/com/lh/learning/leetcode/no65/Question65Test.java
@@ -0,0 +1,24 @@
+package com.lh.learning.leetcode.no65;
+
+import com.lh.learning.leetcode.no64.Question64;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @auther: loneyfall
+ * @date: 2021/4/8
+ * @description:
+ */
+public class Question65Test {
+    private static Question64 question64;
+
+    @BeforeClass
+    public static void beforeClass() {
+        question64 = new Question64();
+    }
+
+    @Test
+    public void test() {
+
+    }
+}

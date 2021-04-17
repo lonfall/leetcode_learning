87f6de3aebfbd4ebeb0779547b93c63641184a75
loneyfall <312988970@qq.com>
2021-04-17 17:14:34 +0800

    no65

--- src/test/java/com/lh/learning/leetcode/no65/Question65Test.java
+++ src/test/java/com/lh/learning/leetcode/no65/Question65Test.java
@@ -1,6 +1,7 @@
 package com.lh.learning.leetcode.no65;
 
-import com.lh.learning.leetcode.no64.Question64;
+import com.alibaba.fastjson.JSON;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -10,15 +11,19 @@ import org.junit.Test;
  * @description:
  */
 public class Question65Test {
-    private static Question64 question64;
+    private static Question65 question65;
 
     @BeforeClass
     public static void beforeClass() {
-        question64 = new Question64();
+        question65 = new Question65();
     }
 
     @Test
     public void test() {
-
+        Assert.assertTrue(question65.isNumber(".1"));
+        Assert.assertTrue(question65.isNumber("0"));
+        Assert.assertFalse(question65.isNumber("e"));
+        Assert.assertFalse(question65.isNumber("."));
+        Assert.assertFalse(question65.isNumber("0e"));
     }
 }

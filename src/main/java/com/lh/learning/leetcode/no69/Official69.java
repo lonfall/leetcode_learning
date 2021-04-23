2192fecb7aa54fed2079e7d778148892fc675b5e
loneyfall <312988970@qq.com>
2021-04-23 17:33:58 +0800

    no69

---
+++ src/main/java/com/lh/learning/leetcode/no69/Official69.java
@@ -0,0 +1,22 @@
+package com.lh.learning.leetcode.no69;
+
+/**
+ * @auther: loneyfall
+ * @date: 2021/4/23
+ * @description: x 的平方根
+ */
+public class Official69 {
+    public int mySqrt(int x) {
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
